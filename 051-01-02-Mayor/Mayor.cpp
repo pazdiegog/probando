fcea50eea4692d32c1a903393483c39e20a8ae03
Diego Paz <paz.diegog@gmail.com>
2017-05-04 18:48:06 +0200

    Add files via upload

---
+++ 051-01-02-Mayor/Mayor.cpp
@@ -0,0 +1,21 @@
+/* Mayor
+ * NGG + AC
+ * 08042017
+ */
+
+#include <iostream>
+
+int main () {
+    
+    int a,b;
+    std::cin >>a;
+    std::cin >>b;
+    
+    if (a>b) {
+        std::cout <<a;
+    }
+    else {
+        std::cout <<b;
+    }
+    
+}
